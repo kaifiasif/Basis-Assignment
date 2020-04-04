597d588edae0669b936ec46e26ad331417f660ab
Kaifi Asif <kaifiasif@Kaifis-MacBook-Pro.local>
2020-04-04 20:42:09 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import Login from './components/login/login';
+import Nav from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className="App">
-			<Login />
-		</div>
+		<Router>
+			<div className="App">
+				<Switch>
+					<Route exact path="/login" component={Login} />
+					<Redirect exact from="/" to="/login" />
+					<Route path="/dashboard" component={Nav} />
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
